40f2d79aa3a0206a17cd0c014b3e6a2856c31bca
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-07 19:00:01 +0430

    add Gender Entity - 1401/02/17  18:59

---
+++ C1System.DataLayar/Entities/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.DataLayar.Entities;
+
+public class Gender
+{
+    [Key]
+    public int GenderId { get; set; }
+
+    [Display(Name ="جنسیت")]
+    public string GenderName { get; set; }
+}
