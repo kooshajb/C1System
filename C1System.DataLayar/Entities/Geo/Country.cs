d1a982e19f6efd7977652f5bfe2e928befd54a68
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 08:54:33 +0430

    add City, Country, Province Entity  - 1401/02/18   08:54

---
+++ C1System.DataLayar/Entities/Geo/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System.DataLayar.Entities.Geo;
+
+public class Country
+{
+    [Key]
+    [Display(Name = "شناسه")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int CountryId { get; set; }
+    
+    [StringLength(200)]
+    [Display(Name = "نام کشور")]
+    public string CountryName { get; set; }
+}
