1f8b546b90ceac44d74bf34c4ed9c77551552900
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 09:13:42 +0430

    fix Introduction Entity  - 1401/02/18   09:13

--- C1System.DataLayar/Entities/Introduction.cs
+++ C1System.DataLayar/Entities/Introduction.cs
@@ -12,21 +12,26 @@ namespace C1System.DataLayar.Entities
         [Key]
         public Guid IntroductionId { get; set; }
 
-        [Display(Name = "ویدیو")]
-        public string Video { get; set; }
-
-
         [Display(Name = "عنوان")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
-        [MinLength(3, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
+        [MinLength(15, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
         [MaxLength(200, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
         public string Title { get; set; }
-
-
+        
+        [Display(Name = "عنوان فرعی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+        [MinLength(30, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
+        [MaxLength(200, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+        public string? SubTitle { get; set; }
+        
         [Display(Name = "توضیحات")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
-        [MinLength(3, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
-        [MaxLength(200, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
-        public String Desc { get; set; }
+        public string Desc { get; set; }
+        
+        [Display(Name = "ویدئو")]
+        public string Video { get; set; }
+        
+        [Display(Name = "تصویر کاور ویدئو")]
+        public string? VideoCoverImage { get; set; }
     }
 }
