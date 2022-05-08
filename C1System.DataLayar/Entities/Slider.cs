003dde31b4c2ed2bee562406f5c5354c5a4affcf
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-08 09:16:25 +0430

    fix Slider Entity  - 1401/02/18   09:16

--- C1System.DataLayar/Entities/Slider.cs
+++ C1System.DataLayar/Entities/Slider.cs
@@ -12,30 +12,24 @@ namespace C1System.DataLayar.Entities
         [Key]
         public Guid SliderId { get; set; }
 
-        [Display(Name = "تصویر")]
-        public string Picture { get; set; }
-
-
         [Display(Name ="عنوان")]
         [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
-        [MinLength(3 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
-        [MaxLength(200 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+        [MinLength(10 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+        [MaxLength(250 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
         public string Title { get; set; }
-
-
 
         [Display(Name = "عنوان فرعی")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
-        [MinLength(3, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
-        [MaxLength(200, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+        [MinLength(10, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
+        [MaxLength(350, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
         public string SubTitle { get; set; }
-
-
-
+        
         [Display(Name = "توضیحات")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
-        [MinLength(3, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
-        [MaxLength(200, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
-        public String Desc { get; set; }
+        public string Description { get; set; }
+        
+        [Display(Name = "تصویر اسلاید")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+        public string SlideImage { get; set; }
     }
 }
