f030ea9edfd34199def2dbe3f6e0223ac88200f7
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-09 14:51:17 +0430

    fix Chat Entity - 1401/02/19 14:51

--- C1System.DataLayar/Entities/Chat/Chat.cs
+++ C1System.DataLayar/Entities/Chat/Chat.cs
@@ -1,6 +1,35 @@
-﻿namespace C1System.DataLayar.Entities.Chat;
+﻿using System.ComponentModel.DataAnnotations;
+using C1System.DataLayar.Entities.Base;
 
-public class Chat
+namespace C1System.DataLayar.Entities.Chat;
+
+public class Chat : BaseEntity
 {
+    [Key] 
+    public Guid ChatId { get; set; }
     
+    [Display(Name = "شماره چت")]
+    public long ChatNumber { get; set; }
+    
+    [Display(Name = "نام و نام خانوادگی")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(300, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string FullName { get; set; }
+
+    [Display(Name = "موضوع")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(500, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string ChatSubject { get; set; }
+    
+    [Display(Name ="ایمیل")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [EmailAddress(ErrorMessage = "لطفا {0} معتبر وارد کنید")]
+    [MinLength(50 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(1500 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Email { get; set; }
+
+    [Display(Name = "تصویر پروفایل")]
+    public string? ProfilePic { get; set; }
+    
+    //todo ChatFaq
 }
