1681937dcfd33cada50afe5ad5742506f260039e
kooshajb <75574002+kooshajb@users.noreply.github.com>
2022-05-09 09:23:15 +0430

    Add Project Repository 1401/02/19   9:23

--- C1System.Core/Services/project/ProjectRepository.cs
+++ C1System.Core/Services/project/ProjectRepository.cs
@@ -1,5 +1,4 @@
-﻿using C1System.Core.Services.Interface;
-using C1System.DataLayar.Context;
+﻿using C1System.DataLayar.Context;
 using C1System.DataLayar.Entities;
 using System;
 using System.Collections.Generic;
@@ -7,8 +6,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C1System.Core.Services
+namespace C1System.Core.Services.project
 {
+    public interface IProjectService
+    {
+        List<Project> GetAllProject();
+        Project GetProjectById(Guid id);
+        bool AddProject(Project project);
+        bool DeleteProject(Project project);
+        bool UpdateProject(Project project);
+    }
+
     public class ProjectService : IProjectService
     {
         private readonly C1SystemContext _context;
