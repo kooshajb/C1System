e6355fbe69ef859175a24cc1eb377eb9be821a27
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-09 13:05:03 +0430

    fix Portfolio Entity - 1401/02/19 13:04

--- C1System.DataLayar/Entities/Portfolio/Portfolio.cs
+++ C1System.DataLayar/Entities/Portfolio/Portfolio.cs
@@ -7,9 +7,6 @@ public class Portfolio
     [Key]
     public Guid PortfolioId { get; set; }
 
-    [Display(Name = "تصویر")]
-    public string Picture { get; set; }
-    
     [Display(Name ="عنوان خدمت")]
     [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
     [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
@@ -25,16 +22,13 @@ public class Portfolio
     [Display(Name = "توضیحات")]
     [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
     [MinLength(200, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
-    [MaxLength(1000, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
     public string Description { get; set; }
+    
     public bool IsDelete { get; set; }
-
-    [Display(Name = "امتیاز")]
-    public int Score { get; set; }
 
     [Display(Name = "آدرس سایت")]
     [Url(ErrorMessage = "{0} معتبر وارد کنید")]
-    public string SiteAddress { get; set; }
+    public string? SiteAddress { get; set; }
     
     [Display(Name = "نام شرکت")]
     [MinLength(20, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
@@ -46,12 +40,13 @@ public class Portfolio
     [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
     public string CompanyLogo { get; set; }
     
-    [Display(Name = "تصاویر")]
-    public string Media { get; set; }
-    
     [Display(Name = "تصویر شاخص")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
     public string FeatureMedia { get; set; }
     
+    [Display(Name = "تصاویر")]
+    public string? Media { get; set; }
+
     [Display(Name ="امتیازدهی")]
     public int? Point { get; set; }
     
