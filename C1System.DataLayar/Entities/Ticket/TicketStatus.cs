eabb3af1c75815bd281cc4b4442d61b48298d134
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-09 14:01:00 +0430

    add entity TicketStatus - 1401/02/19 14:00

--- C1System.DataLayar/Entities/Ticket/TicketStatus.cs
+++ C1System.DataLayar/Entities/Ticket/TicketStatus.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using C1System.DataLayar.Entities.Base.Enums;
+
 //using C1System.DataLayar.Entities.Base.Enums;
 
 namespace C1System.DataLayar.Entities.Ticket;
@@ -8,9 +10,10 @@ public class TicketStatus
     [Key] 
     public int TicketStatusId { get; set; }
     
-    //[Display(Name = "نام وضعیت تیکت")]
-    //[Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
-    //[StringLength(60, ErrorMessage = "لطفا {0} را وارد کنید.")]
-    ////public TicketStatusEnum TicketStatusName { get; set; }
-
+    [Display(Name = "نام وضعیت تیکت")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+    [StringLength(50, ErrorMessage = "لطفا {0} را وارد کنید.")]
+    public string TicketStatusName { get; set; }
+    
+    //todo - ticket entity
 }
