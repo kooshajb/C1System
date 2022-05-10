715fb6e3e920943bf1d3aadc6cdbee72c72f639b
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-10 12:31:19 +0430

    add CategoryController - 1401/02/20 12:30

---
+++ C1System/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C1System.Areas.Admin.Controllers;
+
+public class CategoryController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
