14a1a0e6da5af066769e9f0936338175259ae2af
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-10 12:31:04 +0430

    add ProductController  - 1401/02/20 12:30

---
+++ C1System/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C1System.Areas.Admin.Controllers;
+
+public class ProductController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
