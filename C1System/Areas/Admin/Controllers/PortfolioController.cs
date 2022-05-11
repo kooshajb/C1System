302aa7e1548332cb5417b141f72078259f20eacb
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-11 15:46:37 +0430

    add PortfolioController - 1401/02/20 15:43

---
+++ C1System/Areas/Admin/Controllers/PortfolioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C1System.Areas.API.Admin.Controllers;
+
+[Area("Admin")]
+public class PortfolioController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
