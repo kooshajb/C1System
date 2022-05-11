b853b61e2f6eea980e1334f20dd3692a2ec89185
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-11 16:06:46 +0430

    add PodcastController Admin - 1401/02/20 16:06

---
+++ C1System/Areas/Admin/Controllers/PodcastController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C1System.Controllers.Api;
+
+[Area("Admin")]
+public class PodcastController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
