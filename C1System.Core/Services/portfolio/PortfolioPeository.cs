92cd773e23f733614561bda50da50096defd4c43
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-12 10:31:40 +0430

    add PortfolioPeository - 1401/02/23 10:25

--- C1System.Core/Services/portfolio/PortfolioPeository.cs
+++ C1System.Core/Services/portfolio/PortfolioPeository.cs
@@ -5,91 +5,94 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using C1System.Core.Dtos.Category;
+using C1System.DataLayar.Entities.Responses;
+using AutoMapper;
+using C1System.Core.Dtos.Portfolio;
+using C1System.DataLayar.Entities.Utilities.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
 
 namespace C1System.Core.Services.portfolio
 {
-    public interface IPortfolioService
+    public interface IPortfolioRepository
     {
-        List<Portfolio> GetAllPortfolio();
-        Portfolio GetPortfolioById(Guid id);
-        bool AddPortfolio(Portfolio portfolio);
-        bool DeletePortfolio(Portfolio portfolio);
-        bool UpdatePortfolio(Portfolio portfolio);
+        Task<GenericResponse<GetPortfolioDto>> Add(AddUpdatePortfolioDto dto);
+        Task<GenericResponse<IEnumerable<GetPortfolioDto>>> Get();
+        Task<GenericResponse<GetPortfolioDto>> GetById(Guid id);
+        Task<GenericResponse<GetPortfolioDto>> Update(Guid id, AddUpdatePortfolioDto dto);
+        Task<GenericResponse> Delete(Guid id);
+        bool ExistPortfolio(string title, Guid portfoiloId);
     }
 
-    public class PortfolioService : IPortfolioService
+    public class PortfolioRepository : IPortfolioRepository
     {
-        private C1SystemContext _context;
-        public PortfolioService(C1SystemContext context)
+        private readonly C1SystemContext _context;
+        private readonly IMapper _mapper;
+
+        public PortfolioRepository(C1SystemContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
-        public List<Portfolio> GetAllPortfolio()
+        public async Task<GenericResponse<GetPortfolioDto>> Add(AddUpdatePortfolioDto dto)
         {
-            return _context.Portfolios.ToList();
+            if (dto == null) throw new ArgumentException("Dto must not be null", nameof(dto));
+            Portfolio entity = _mapper.Map<Portfolio>(dto);
+
+            EntityEntry<Portfolio> i = await _context.Set<Portfolio>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return new GenericResponse<GetPortfolioDto>(_mapper.Map<GetPortfolioDto>(i.Entity));
         }
 
-        public Portfolio GetPortfolioById(Guid id)
+        public async Task<GenericResponse<IEnumerable<GetPortfolioDto>>> Get()
         {
-            return _context.Portfolios.Find(id);
+            IEnumerable<Portfolio> i = await _context.Set<Portfolio>().AsNoTracking().ToListAsync();
+            return new GenericResponse<IEnumerable<GetPortfolioDto>>(_mapper.Map<IEnumerable<GetPortfolioDto>>(i));
         }
 
-        public bool AddPortfolio(Portfolio portfolio)
+        public async Task<GenericResponse<GetPortfolioDto>> GetById(Guid id)
         {
-            try
-            {
-                _context.Portfolios.Add(portfolio);
-                _context.SaveChanges();
-                return true;
-            }
-            catch (Exception)
-            {
-
-                return false;
-            }
+            Portfolio? i = await _context.Set<Portfolio>().AsNoTracking()
+                .FirstOrDefaultAsync(i => i.PortfolioId == id);
+            return new GenericResponse<GetPortfolioDto>(_mapper.Map<GetPortfolioDto>(i));
         }
 
-        public bool DeletePortfolio(Portfolio portfolio)
+        public async Task<GenericResponse<GetPortfolioDto>> Update(Guid id, AddUpdatePortfolioDto dto)
         {
-            if (portfolio != null)
-            {
-                try
-                {
-                    _context.Portfolios.Remove(portfolio);
-                    _context.SaveChanges();
-                    return true;
-                }
-                catch (Exception)
-                {
+            var i = _context.Set<Portfolio>()
+                .Where(p => p.PortfolioId == id).First();
 
-                    return false;
-                }
-            }
-            else
-                return false;
+            i.Title = dto.Title;
+            i.SubTitle = dto.SubTitle;
+            i.Description = dto.Description;
+            i.SiteAddress = dto.SiteAddress;
+            i.CompanyName = dto.CompanyName;
+            i.CompanyLogo = dto.CompanyLogo;
+            i.FeatureMedia = dto.FeatureMedia;
+            i.Media = dto.Media;
+            i.Point = dto.Point;
+
+            _context.Set<Portfolio>().Update(i);
+            await _context.SaveChangesAsync();
+            return new GenericResponse<GetPortfolioDto>(_mapper.Map<GetPortfolioDto>(i));
         }
 
-        public bool UpdatePortfolio(Portfolio portfolio)
+        public async Task<GenericResponse> Delete(Guid id)
         {
-            if (portfolio != null)
-            {
-                try
-                {
-                    _context.Portfolios.Update(portfolio);
-                    _context.SaveChanges();
-                    return true;
-                }
-                catch (Exception)
-                {
-
-                    throw;
-                }
-            }
-            else
-                return false;
+            GenericResponse<GetPortfolioDto> i = await GetById(id);
+            _context.Set<Portfolio>().Remove(_mapper.Map<Portfolio>(i.Result));
+            await _context.SaveChangesAsync();
+            return new GenericResponse(UtilitiesStatusCodes.Success,
+                $"Portfolio {i.Result.Title} delete Success {i.Result.PortfolioId}");
         }
-
-
+        
+        public bool ExistPortfolio(string title, Guid portfoiloId)
+        {
+            return _context.Portfolios.Any(p =>
+                p.Title == title && p.PortfolioId != portfoiloId);
+        }
     }
 }
