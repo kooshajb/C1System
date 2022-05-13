779928891296a157b857aec870355d2b4c21f286
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-13 18:10:52 +0430

    fix CareerOpportunityRepository

--- C1System/Dtos/CareerOpportunity/CareerOpportunityRepository.cs
+++ C1System/Dtos/CareerOpportunity/CareerOpportunityRepository.cs
@@ -7,82 +7,8 @@ using System.Threading.Tasks;
 namespace C1System;
 public interface ICareerOpportunityRepository
 {
-    //List<CareerOpportunity> GetAllCareerOpportunity();
-    //CareerOpportunity GetCareerOpportunityById(Guid id);
-    //bool AddCareerOpportunity(CareerOpportunity careerOpportunity);
-    //bool DeleteCareerOpportunity(CareerOpportunity careerOpportunity);
-    //bool UpdateCareerOpportunity(CareerOpportunity careerOpportunity);
 }
 public class CareerOpportunityRepository : ICareerOpportunityRepository
 {
-    //private readonly C1SystemContext _context;
-    //public CareerOpportunityService(C1SystemContext context)
-    //{
-    //    _context = context;
-    //}
 
-    //public bool AddCareerOpportunity(CareerOpportunity careerOpportunity)
-    //{
-    //    try
-    //    {
-    //        _context.CareerOpportunities.Add(careerOpportunity);
-    //        _context.SaveChanges();
-    //        return true;
-    //    }
-    //    catch (Exception)
-    //    {
-
-    //        return false;
-    //    }
-    //}
-
-    //public bool DeleteCareerOpportunity(CareerOpportunity careerOpportunity)
-    //{
-    //    if (careerOpportunity != null)
-    //    {
-    //        try
-    //        {
-    //            _context.CareerOpportunities.Remove(careerOpportunity);
-    //            _context.SaveChanges();
-    //            return true;
-    //        }
-    //        catch (Exception)
-    //        {
-
-    //            return false;
-    //        }
-    //    }
-    //    else
-    //        return false;
-    //}
-
-    //public List<CareerOpportunity> GetAllCareerOpportunity()
-    //{
-    //    return _context.CareerOpportunities.ToList();
-    //}
-
-    //public CareerOpportunity GetCareerOpportunityById(Guid id)
-    //{
-    //    return _context.CareerOpportunities.Find(id);
-    //}
-
-    //public bool UpdateCareerOpportunity(CareerOpportunity careerOpportunity)
-    //{
-    //    if (careerOpportunity != null)
-    //    {
-    //        try
-    //        {
-    //            _context.CareerOpportunities.Update(careerOpportunity);
-    //            _context.SaveChanges();
-    //            return true;
-    //        }
-    //        catch (Exception)
-    //        {
-
-    //            throw;
-    //        }
-    //    }
-    //    else
-    //        return false;
-    //}
 }
