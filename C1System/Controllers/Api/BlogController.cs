f043ae3dc9421cc9e432837d9c7b12e79de58fc6
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-13 20:53:27 +0430

    add BlogController

---
+++ C1System/Controllers/Api/BlogController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C1System.Controllers.Api;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
+{
+    private readonly IBlogRepository _blogRepository;
+
+    public BlogController(IBlogRepository blogRepository)
+    {
+        _blogRepository = blogRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<GenericResponse<IEnumerable<GetBlogDto>>>> GetBlogs()
+    {
+        var blogs = await _blogRepository.Get();
+        return Ok(blogs.Result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreatePodcast([FromBody]AddUpdateBlogDto dto)
+    {
+        if (dto == null)  return BadRequest();
+        var createdBlog = await _blogRepository.Add(dto);
+        return CreatedAtAction(nameof(GetBlog), new { id = createdBlog.Id}, createdBlog.Result);
+    }
+    
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<GetPortfolioDto>> GetBlog(Guid id)
+    {
+        var blog = await _blogRepository.GetById(id);
+
+        if (blog == null) return NotFound();
+
+        return Ok(blog.Result);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult<GetPortfolioDto>> UpdatePodcast([FromBody]AddUpdateBlogDto dto, Guid id)
+    {
+        var blog =  await _blogRepository.Update(id, dto);
+        return Ok(blog.Result);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> DeletePodcast(Guid id)
+    {
+        await _blogRepository.Delete(id);
+        return NoContent();
+    }
+}
