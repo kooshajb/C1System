38691fb78cd2e97da4cc0a660be6f7337fd60628
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-13 18:08:33 +0430

    fix UserRepository namespace

--- C1System/Data/User/UserRepository.cs
+++ C1System/Data/User/UserRepository.cs
@@ -1,17 +1,14 @@
-﻿using C1System.DataLayar.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C1System.Core.Services.user
+namespace C1System;
+public interface IUserRepository
 {
-    public interface IUserRepository
-    {
-    }
+}
 
-    public class UserRepository : IUserRepository
-    {
-    }
+public class UserRepository : IUserRepository
+{
 }
