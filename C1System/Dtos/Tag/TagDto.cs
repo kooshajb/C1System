86f3df9aaebe038b9edd3e0803f775b9c3a03bee
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-13 18:22:17 +0430

    Fix TagDto namespace

--- C1System/Dtos/Tag/TagDto.cs
+++ C1System/Dtos/Tag/TagDto.cs
@@ -1,15 +1,16 @@
-﻿namespace C1System.Dtos.Tag
-{
-    public class GetTagDto
-    {
-        public Guid TagId { get; set; }
-        public string Title { get; set; }
-        public string Link { get; set; }
-    }
+﻿using System;
 
-    public class AddUpdateTagDto
-    {
-        public string Title { get; set; }
-        public string Link { get; set; }
-    }
+namespace C1System;
+
+public class GetTagDto
+{
+    public Guid TagId { get; set; }
+    public string Title { get; set; }
+    public string Link { get; set; }
+}
+
+public class AddUpdateTagDto
+{
+    public string Title { get; set; }
+    public string Link { get; set; }
 }
