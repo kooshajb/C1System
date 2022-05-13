8b7a7bb9f494e39178e95ab774e4d9e52a9b318b
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-13 20:52:23 +0430

    add Category_Product entity

---
+++ C1System/Models/Category_Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C1System;
+
+public class Category_Product
+{
+    [Key]
+    public Guid CategoryProductId { get; set; }
+    public Guid PortfolioId { get; set; }
+    public Guid CategoryId { get; set; }
+
+    #region Relation
+
+    [ForeignKey("CategoryId")]
+    public Category Category { get; set; }
+    
+    [ForeignKey("PortfolioId")]
+    public Portfolio Portfolio { get; set; }
+
+    #endregion
+}
