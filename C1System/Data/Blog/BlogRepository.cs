74af8192b953e8ce22c8aed8d36e3769f12150c7
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-13 16:55:31 +0430

    fix BlogRepository

--- C1System/Data/Blog/BlogRepository.cs
+++ C1System/Data/Blog/BlogRepository.cs
@@ -1,90 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace C1System;
 
 public interface IBlogRepository
 {
-    //List<Blog> GetAllBlog();
-    //Blog GetBlogById(Guid id);
-    //bool AddBlog(Blog blog);
-    //bool DeleteBlog(Blog blog);
-    //bool UpdateBlog(Blog blog);
+    Task<GenericResponse<GetBlogDto>> Add(AddUpdateBlogDto dto);
+    Task<GenericResponse<IEnumerable<GetBlogDto>>> Get();
+    Task<GenericResponse<GetBlogDto>> GetById(Guid id);
+    Task<GenericResponse<GetBlogDto>> Update(Guid id, AddUpdateBlogDto dto);
+    Task<GenericResponse> Delete(Guid id);
+    bool ExistBlog(string title, Guid blogId);
 }
 
 public class BlogRepository : IBlogRepository
 {
-//    private readonly C1SystemContext _context;
-//    public BlogInterface(C1SystemContext context)
-//    {
-//        _context = context;
-//    }
+    private C1SystemContext _context;
+    private readonly IMapper _mapper;
 
-//    public bool AddBlog(Blog blog)
-//    {
-//        try
-//        {
-//            _context.Blogs.Add(blog);
-//            _context.SaveChanges();
-//            return true;
-//        }
-//        catch (Exception)
-//        {
+    public BlogRepository(C1SystemContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
 
-//            return false;
-//        }
-//    }
+    public async Task<GenericResponse<GetBlogDto>> Add(AddUpdateBlogDto dto)
+    {
+        if (dto == null) throw new ArgumentException("Dto must not be null", nameof(dto));
+        Blog blog = _mapper.Map<Blog>(dto);
 
-//    public bool DeleteBlog(Blog blog)
-//    {
-//        if (blog != null)
-//        {
-//            try
-//            {
-//                _context.Blogs.Remove(blog);
-//                _context.SaveChanges();
-//                return true;
-//            }
-//            catch (Exception)
-//            {
+        EntityEntry<Blog> i = await _context.Set<Blog>().AddAsync(blog);
+        await _context.SaveChangesAsync();
+        return new GenericResponse<GetBlogDto>(_mapper.Map<GetBlogDto>(i.Entity));
+    }
 
-//                return false;
-//            }
-//        }
-//        else
-//            return false;
-//    }
+    public async Task<GenericResponse<IEnumerable<GetBlogDto>>> Get()
+    {
+        IEnumerable<Blog> i = await _context.Set<Blog>().AsNoTracking().ToListAsync();
+        return new GenericResponse<IEnumerable<GetBlogDto>>(_mapper.Map<IEnumerable<GetBlogDto>>(i));
+    }
+    
+    public async Task<GenericResponse<GetBlogDto>> GetById(Guid id)
+    {
+        Blog? i = await _context.Set<Blog>().AsNoTracking()
+            .FirstOrDefaultAsync(i => i.BlogId == id);
+        return new GenericResponse<GetBlogDto>(_mapper.Map<GetBlogDto>(i));
+    }
 
-//    public List<Blog> GetAllBlog()
-//    {
-//        return _context.Blogs.ToList();
-//    }
+    public async Task<GenericResponse<GetBlogDto>> Update(Guid id, AddUpdateBlogDto dto)
+    {
+        var i = _context.Set<Blog>()
+            .Where(p => p.BlogId == id).First();
 
-//    public Blog GetBlogById(Guid id)
-//    {
-//        return _context.Blogs.Find(id);
-//    }
+        i.Title = dto.Title;
+        i.Description = dto.Description;
+        i.Lid = dto.Lid;
+        i.StudyTime = dto.StudyTime;
+        i.FeatureImage = dto.FeatureImage;
 
-//    public bool UpdateBlog(Blog blog)
-//    {
-//        if (blog != null)
-//        {
-//            try
-//            {
-//                _context.Blogs.Update(blog);
-//                _context.SaveChanges();
-//                return true;
-//            }
-//            catch (Exception)
-//            {
+        _context.Set<Blog>().Update(i);
+        await _context.SaveChangesAsync();
+        return new GenericResponse<GetBlogDto>(_mapper.Map<GetBlogDto>(i));
+    }
 
-//                throw;
-//            }
-//        }
-//        else
-//            return false;
-//    }
+    public async Task<GenericResponse> Delete(Guid id)
+    {
+        GenericResponse<GetBlogDto> i = await GetById(id);
+        _context.Set<Blog>().Remove(_mapper.Map<Blog>(i.Result));
+        await _context.SaveChangesAsync();
+        return new GenericResponse(UtilitiesStatusCodes.Success,
+            $"Blog {i.Result.Title} delete Success {i.Result.BlogId}");
+    }
+
+    public bool ExistBlog(string title, Guid blogId)
+    {
+        return _context.Blogs.Any(b =>
+            b.Title == title && b.BlogId != blogId);
+    }
 }
