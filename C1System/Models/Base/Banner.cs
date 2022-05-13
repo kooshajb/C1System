0131ce4bb717f5e1f9d9f6bf15f859fb85d4d4f2
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-13 12:14:44 +0430

    edit Banner entity

--- C1System/Models/Base/Banner.cs
+++ C1System/Models/Base/Banner.cs
@@ -4,26 +4,26 @@ namespace C1System.DataLayar.Entities;
 
 public class Banner
 {
-    [Key]
-    public Guid SliderId { get; set; }
+    //[Key]
+    //public Guid SliderId { get; set; }
 
-    [Display(Name ="عنوان")]
-    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
-    [MinLength(10 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
-    [MaxLength(250 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
-    public string Title { get; set; }
+    //[Display(Name ="عنوان")]
+    //[Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    //[MinLength(10 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    //[MaxLength(250 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    //public string Title { get; set; }
 
-    [Display(Name = "عنوان فرعی")]
-    [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
-    [MinLength(10, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
-    [MaxLength(350, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
-    public string SubTitle { get; set; }
+    //[Display(Name = "عنوان فرعی")]
+    //[Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+    //[MinLength(10, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
+    //[MaxLength(350, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    //public string SubTitle { get; set; }
         
-    [Display(Name = "توضیحات")]
-    [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
-    public string Description { get; set; }
+    //[Display(Name = "توضیحات")]
+    //[Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+    //public string Description { get; set; }
         
-    [Display(Name = "تصویر اسلاید")]
-    [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
-    public string SlideImage { get; set; }
+    //[Display(Name = "تصویر اسلاید")]
+    //[Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
+    //public string SlideImage { get; set; }
 }
