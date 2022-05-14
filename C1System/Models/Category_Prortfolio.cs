69b581346a7ae4db79fb441f6bccb3901750adc9
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-14 14:14:29 +0430

    add Category_Portfolio entity

---
+++ C1System/Models/Category_Prortfolio.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C1System;
+
+public class Category_Portfolio
+{
+    [Key]
+    public Guid CategoryPortfolioId { get; set; }
+    public Guid PortfolioId { get; set; }
+    public Guid CategoryId { get; set; }
+
+    #region Relation
+    
+    [ForeignKey("CategoryId")]
+    public Category Category { get; set; }
+    
+    [ForeignKey("PortfolioId")]
+    public Portfolio Portfolio { get; set; }
+
+    #endregion
+}
