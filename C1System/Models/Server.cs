5ee642a92a5b1162dc3aedee7de8cc5f7da8c94f
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-14 16:44:52 +0430

    add server class

---
+++ C1System/Models/Server.cs
@@ -0,0 +1,24 @@
+﻿
+namespace C1System;
+
+public class Server {
+    private static IHttpContextAccessor? _httpContextAccessor;
+
+    public static void Configure(IHttpContextAccessor? httpContextAccessor) {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    private static string? _serverAddress;
+
+    public static string ServerAddress {
+        get {
+            if (_serverAddress != null) return _serverAddress;
+            HttpRequest? request = _httpContextAccessor?.HttpContext?.Request;
+            string? scheme = request?.Scheme;
+            string? host = request?.Host.ToUriComponent();
+            string? pathBase = request?.PathBase.ToUriComponent();
+            _serverAddress = $"{scheme}://{host}{pathBase}";
+            return _serverAddress;
+        }
+    }
+}
