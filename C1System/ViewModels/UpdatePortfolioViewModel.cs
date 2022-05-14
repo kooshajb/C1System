d6272b041ea7e4e30da7decae16cac85620da92b
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-14 14:14:07 +0430

    Add UpdatePortfolioViewModel

---
+++ C1System/ViewModels/UpdatePortfolioViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.ViewModels;
+
+public class UpdatePortfolioViewModel
+{
+    public Guid PortfolioId { get; set; }
+
+    [Display(Name = "عنوان نمونه کار")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    public string PortfolioTitle { get; set; }
+    
+    public Guid CategoryPortfolioId { get; set; }
+    public Guid CategoryId { get; set; }
+}
