df6ac93b1c655ff98eeb1cccb2a747b60ed2cfb8
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-14 14:15:59 +0430

    fix PortfolioDto

--- C1System/Dtos/Portfolio/PortfolioDto.cs
+++ C1System/Dtos/Portfolio/PortfolioDto.cs
@@ -16,8 +16,22 @@ public class GetPortfolioDto
     public int? Point { get; set; }
 }
 
-public class AddUpdatePortfolioDto
+public class AddPortfolioDto
 {
+    public string Title { get; set; }
+    public string SubTitle { get; set; }
+    public string Description { get; set; }
+    public string? SiteAddress { get; set; }
+    public string CompanyName { get; set; }
+    public string CompanyLogo { get; set; }
+    public string FeatureMedia { get; set; }
+    public string? Media { get; set; }
+}
+
+
+public class UpdatePortfolioDto
+{ 
+    public Guid PortfolioId { get; set; }
     public string Title { get; set; }
     public string SubTitle { get; set; }
     public string Description { get; set; }
@@ -31,8 +45,10 @@ public class AddUpdatePortfolioDto
 
 public class AutoMapperPortfolio : Profile {
     public AutoMapperPortfolio() {
-        CreateMap<Portfolio, AddUpdatePortfolioDto>().ReverseMap();
+        CreateMap<Portfolio, AddPortfolioDto>().ReverseMap();
+        CreateMap<Portfolio, UpdatePortfolioDto>().ReverseMap();
         CreateMap<Portfolio, GetPortfolioDto>().ReverseMap();
-        CreateMap<AddUpdatePortfolioDto, GetPortfolioDto>().ReverseMap();
+        CreateMap<AddPortfolioDto, GetPortfolioDto>().ReverseMap();
+        CreateMap<UpdatePortfolioDto, GetPortfolioDto>().ReverseMap();
     }
 }
