a37c815722600c5f63924165f933b9cfee650172
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-15 14:55:45 +0430

    fix BookMarkEntity

--- C1System/Models/BookMark/BookMarkEntity.cs
+++ C1System/Models/BookMark/BookMarkEntity.cs
@@ -9,4 +9,8 @@ public class BookMarkEntity : BaseEntity
     //todo - blog 
     //todo - podcast 
     //todo -portfolio
+
+    public List<BlogEntity> Blogs { get; set; }
+    public List<PodcastEntity> Podcasts { get; set; }
+    public List<PortfolioEntity> Portfolios { get; set; }
 }
