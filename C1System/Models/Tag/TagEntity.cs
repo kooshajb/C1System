5240054cb0dead2f2683631726a4dd7a194731e2
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-16 01:20:28 +0430

    fix TagEntity

--- C1System/Models/Tag/TagEntity.cs
+++ C1System/Models/Tag/TagEntity.cs
@@ -12,13 +12,13 @@ public class TagEntity
     
     [Display(Name ="عنوان تگ")]
     [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
-    [MinLength(30 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
     [MaxLength(150 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
     public string Title { get; set; }
     
     [Display(Name ="لینک تگ")]
     [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
-    [MinLength(150 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
     public string Link { get; set; }
 
     #region Relation
