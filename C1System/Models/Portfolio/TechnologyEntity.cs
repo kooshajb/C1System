377025c49b6980bc4fde6c63f6e362e380fcef3d
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-15 11:57:57 +0430

    add TechnologyEntity

---
+++ C1System/Models/Portfolio/TechnologyEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System;
+
+[Table("Technology")]
+public class TechnologyEntity
+{
+    [Key]
+    public Guid TechnologyPortfolioId { get; set; }
+    
+    [Display(Name ="نام تکنولوژی")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(250 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Title { get; set; }
+    
+    [Display(Name = "تصویر تکنولوژی")]
+    public string TechnologyImage { get; set; }
+
+    #region Relation
+
+    public List<Portfolio_Technology> PortfolioTechnologies { get; set; }
+
+    #endregion
+}
