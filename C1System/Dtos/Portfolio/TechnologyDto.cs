ee30ee43f5ed6243eb14b2cb95542ecfff089956
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-15 12:40:24 +0430

    fix TechnologyDto

--- C1System/Dtos/Portfolio/TechnologyDto.cs
+++ C1System/Dtos/Portfolio/TechnologyDto.cs
@@ -4,30 +4,21 @@ using C1System;
 
 public class GetTechnologyDto
 {
+    public Guid TechnologyId { get; set; }
     public string Title { get; set; }
     public string TechnologyImage { get; set; }
 }
 
-public class AddTechnologyDto
+public class AddUpdateTechnologyDto
 {
-    public Guid TechnologyPortfolioId { get; set; }
-    public string Title { get; set; }
-    public string TechnologyImage { get; set; }
-}
-
-public class UpdateTechnologyDto
-{
-    public Guid TechnologyPortfolioId { get; set; }
     public string Title { get; set; }
     public string TechnologyImage { get; set; }
 }
 
 public class AutoMapperTechnology : Profile {
     public AutoMapperTechnology() {
-        CreateMap<TechnologyEntity, AddTechnologyDto>().ReverseMap();
-        CreateMap<TechnologyEntity, UpdateTechnologyDto>().ReverseMap();
+        CreateMap<TechnologyEntity, AddUpdateTechnologyDto>().ReverseMap();
         CreateMap<TechnologyEntity, GetTechnologyDto>().ReverseMap();
-        CreateMap<AddTechnologyDto, GetTechnologyDto>().ReverseMap();
-        CreateMap<UpdateTechnologyDto, GetTechnologyDto>().ReverseMap();
+        CreateMap<AddUpdateTechnologyDto, GetTechnologyDto>().ReverseMap();
     }
 }
