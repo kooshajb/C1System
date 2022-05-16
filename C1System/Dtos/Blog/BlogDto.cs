4b905d56dc239c892e95a88270592ca1d464f0bf
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-16 11:47:27 +0430

    add blog dto

--- C1System/Dtos/Blog/BlogDto.cs
+++ C1System/Dtos/Blog/BlogDto.cs
@@ -11,7 +11,16 @@ public class GetBlogDto
     public string FeatureImage { get; set; }
 }
 
-public class AddUpdateBlogDto
+public class AddBlogDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Lid { get; set; }
+    public string StudyTime { get; set; }
+    public string FeatureImage { get; set; }
+}
+
+public class UpdateBlogDto
 {
     public Guid BlogId { get; set; }
     public string Title { get; set; }
@@ -21,10 +30,13 @@ public class AddUpdateBlogDto
     public string FeatureImage { get; set; }
 }
 
+
 public class AutoMapperBlog : Profile {
     public AutoMapperBlog() {
-        CreateMap<BlogEntity, AddUpdateBlogDto>().ReverseMap();
+        CreateMap<BlogEntity, AddBlogDto>().ReverseMap();
+        CreateMap<BlogEntity, UpdateBlogDto>().ReverseMap();
         CreateMap<BlogEntity, GetBlogDto>().ReverseMap();
-        CreateMap<AddUpdateBlogDto, GetBlogDto>().ReverseMap();
+        CreateMap<AddBlogDto, GetBlogDto>().ReverseMap();
+        CreateMap<UpdateBlogDto, GetBlogDto>().ReverseMap();
     }
 }
