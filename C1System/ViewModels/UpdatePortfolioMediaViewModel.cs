bc35b9a2f52b602c90afe9ab4b9fc148ea1cc47a
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-17 12:04:42 +0430

    add UpdatePortfolioMediaViewModel

---
+++ C1System/ViewModels/UpdatePortfolioMediaViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.ViewModels;
+
+public class UpdatePortfolioMediaViewModel
+{
+    public Guid PortfolioId { get; set; }
+    public string FileName { get; set; }
+}
