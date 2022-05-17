aa44e95ad08b9595cc8a762fe732ff79d936e7d3
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-17 18:53:48 +0430

    fix ProjectEntity

--- C1System/Models/Project/ProjectEntity.cs
+++ C1System/Models/Project/ProjectEntity.cs
@@ -29,11 +29,10 @@ public class ProjectEntity
     [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
     [MinLength(200, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
     [MaxLength(1000, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
-    public string Desc { get; set; }
+    public string Description { get; set; }
 
     public bool IsDelete { get; set; }
     
     [Display(Name = "تصاویر")]
     public string Media { get; set; }
-
 }
