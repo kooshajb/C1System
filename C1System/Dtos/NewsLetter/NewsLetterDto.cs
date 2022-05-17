397e4e49a566d7e38e80dd0587585561bb7019d5
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-17 18:55:05 +0430

    add NewsLetterDto

---
+++ C1System/Dtos/NewsLetter/NewsLetterDto.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using C1System;
+
+public class GetNewsLetterDto
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+} 
+
+public class AddUpdateNewsLetterDto
+{
+    public Guid NewsLetterId { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+}
+
+public class AutoMapperNewsLetter : Profile {
+    public AutoMapperNewsLetter() {
+        CreateMap<NewsLetterEntity, AddUpdateNewsLetterDto>().ReverseMap();
+        CreateMap<NewsLetterEntity, GetNewsLetterDto>().ReverseMap();
+        CreateMap<AddUpdateNewsLetterDto, GetNewsLetterDto>().ReverseMap();
+    }
+}
