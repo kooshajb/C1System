e39efb4f51f73adf2d220edaa0bf1c7660d9d150
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-17 18:51:14 +0430

    add AdminNewsLetterController

---
+++ C1System/Areas/Admin/Controllers/AdminNewsLetterController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C1System.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class AdminNewsLetterController : Controller
+{
+ private readonly INewsLetterRepository _newsLetterRepository;
+
+    public AdminNewsLetterController(INewsLetterRepository newsLetterRepository)
+    {
+        _newsLetterRepository = newsLetterRepository;
+    }
+    
+    public async Task<IActionResult> Index()
+    {
+        var model = await _newsLetterRepository.Get();
+        return View(model.Result);
+    }
+    
+    [HttpGet]
+    public IActionResult AddNewsLetter(Guid id)
+    {
+        if (id != null)
+        {
+            ViewBag.Id = id;
+        }
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> AddNewsLetter(AddUpdateNewsLetterDto dto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(dto);
+        }
+        // if (_newsLetterRepository.ExistNewsLetter(dto.Email,0))
+        // {
+        //     ModelState.AddModelError("ErrorNewsLetter", "ایمیل تکراری است");
+        //     return View(dto);
+        // }
+        
+        var newsLetter = await _newsLetterRepository.Add(dto);
+        Guid newsLetterId = newsLetter.Result.NewsLetterId;
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdateNewsLetter(Guid id)
+    {
+        var newsLetter = await _newsLetterRepository.GetById(id);
+        if (newsLetter.Result == null)
+        {
+            TempData["NotFoundNewsLetter"] = "true";
+            return RedirectToAction(nameof(Index));
+        }
+        return View(newsLetter.Result);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> UpdateNewsLetter(AddUpdateNewsLetterDto dto, Guid id)
+    {
+        var newsLetter = await _newsLetterRepository.GetById(id);
+        if (!ModelState.IsValid)
+        {
+            return View(newsLetter.Result);
+        }
+        
+        var updateNewsLetter = await _newsLetterRepository.Update(id, dto);
+        
+        TempData["Result"] = updateNewsLetter.Result != null ? "true" : "false";
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> DeleteNewsLetter(Guid id)
+    {
+        var newsLetter = await _newsLetterRepository.GetById(id);
+        if (newsLetter.Result == null)
+        {
+            TempData["NotFoundNewsLetter"] = true;
+            return RedirectToAction(nameof(Index));
+        }
+        return View(newsLetter.Result);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> DeleteNewsLetterById(Guid id)
+    {
+        var response = await _newsLetterRepository.Delete(id);
+        TempData["ResultDelete"] = response.Status == UtilitiesStatusCodes.Success ? "true" : "false";
+        return RedirectToAction(nameof(Index));
+    }
+}
