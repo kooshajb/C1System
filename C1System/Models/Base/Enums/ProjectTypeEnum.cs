2266819ca62a569dab0a55276b827a48a19c3a1a
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 15:28:03 +0430

    add ProjectTypeEnum

---
+++ C1System/Models/Base/Enums/ProjectTypeEnum.cs
@@ -0,0 +1,19 @@
+﻿namespace C1System;
+
+public enum ProjectTypeEnum
+{
+    Frontend = 0,
+    Backend = 1,
+    Android = 2,
+    Ios = 3,
+    MobileFlutter = 4,
+    WebApplication = 5,
+    Pwa = 6,
+    Website = 7,
+    UiUx = 8,
+    AugmentedReality = 9,
+    VirtualReality = 10,
+    Modeling = 11,
+    DigitalMarketing = 12,
+    Consulting = 13
+}
