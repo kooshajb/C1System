ef794178cf293a4203ed199d22c509abe9a41641
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 17:52:05 +0430

    fix TechnologyDto

--- C1System/Dtos/Portfolio/TechnologyDto.cs
+++ C1System/Dtos/Portfolio/TechnologyDto.cs
@@ -5,14 +5,12 @@ using C1System;
 public class GetTechnologyDto
 {
     public Guid TechnologyId { get; set; }
-    public string Title { get; set; }
-    public string TechnologyImage { get; set; }
+    public string Title { get; set; } 
 }
 
 public class AddUpdateTechnologyDto
 {
     public string Title { get; set; }
-    public string TechnologyImage { get; set; }
 }
 
 public class AutoMapperTechnology : Profile {
