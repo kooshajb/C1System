82ff10742c17054b7664fb616819baf34eff0588
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 15:26:16 +0430

    add ProjectStatusEntity

---
+++ C1System/Models/Project/ProjectStatusEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System;
+
+[Table("ProjectStatus")]
+public class ProjectStatusEntity
+{
+    [Key]
+    public Guid ProjectStatusId { get; set; }
+    
+    
+}
