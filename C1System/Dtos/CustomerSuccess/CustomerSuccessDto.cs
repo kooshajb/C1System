16668fd3ab04a90b34252f7f4d3e4de68c9e06df
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-19 00:51:16 +0430

    fix CustomerSuccessDto

--- C1System/Dtos/CustomerSuccess/CustomerSuccessDto.cs
+++ C1System/Dtos/CustomerSuccess/CustomerSuccessDto.cs
@@ -9,36 +9,45 @@ public class GetCustomerSuccessDto
     public string CompanyName { get; set; }
     public string StartupName { get; set; }
     public string ActivityName { get; set; }
-    public string? CompanyLogo { get; set; }
     public string ManagerSpeech { get; set; }
     public string Description { get; set; }
-    public string? VideoFile { get; set; }
-    public string? CoverVideoImage { get; set; }
     public string? VideoTitle { get; set; }
     public string? VideoSubTitle { get; set; }
 }
 
-public class AddUpdateCustomerSuccessDto
+public class AddCustomerSuccessDto
 {
     public string ManagerName { get; set; }
     public string ManagerSide { get; set; }
     public string CompanyName { get; set; }
     public string StartupName { get; set; }
     public string ActivityName { get; set; }
-    public string? CompanyLogo { get; set; }
     public string ManagerSpeech { get; set; }
     public string Description { get; set; }
-    public string? VideoFile { get; set; }
-    public string? CoverVideoImage { get; set; }
+    public string? VideoTitle { get; set; }
+    public string? VideoSubTitle { get; set; }
+}
+
+public class UpdateCustomerSuccessDto
+{
+    public Guid CustomerSuccessId { get; set; }
+    public string ManagerName { get; set; }
+    public string ManagerSide { get; set; }
+    public string CompanyName { get; set; }
+    public string StartupName { get; set; }
+    public string ActivityName { get; set; }
+    public string ManagerSpeech { get; set; }
+    public string Description { get; set; }
     public string? VideoTitle { get; set; }
     public string? VideoSubTitle { get; set; }
 }
 
 public class AutoMapperCustomerSuccess : Profile {
     public AutoMapperCustomerSuccess() {
-        CreateMap<CustomerSuccessEntity, AddUpdateCustomerSuccessDto>().ReverseMap();
+        CreateMap<CustomerSuccessEntity, AddCustomerSuccessDto>().ReverseMap();
+        CreateMap<CustomerSuccessEntity, UpdateCustomerSuccessDto>().ReverseMap();
         CreateMap<CustomerSuccessEntity, GetCustomerSuccessDto>().ReverseMap();
-        CreateMap<AddUpdateCustomerSuccessDto, GetCustomerSuccessDto>().ReverseMap();
-        CreateMap<AddUpdateCustomerSuccessDto, GetCustomerSuccessDto>().ReverseMap();
+        CreateMap<AddCustomerSuccessDto, GetCustomerSuccessDto>().ReverseMap();
+        CreateMap<UpdateCustomerSuccessDto, GetCustomerSuccessDto>().ReverseMap();
     }
 }
