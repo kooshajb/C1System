44541703a00bf367654f59c125b9198c179ef8a8
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 15:24:40 +0430

    add ProjectTypeEntity

---
+++ C1System/Models/Project/ProjectTypeEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System;
+
+[Table("ProjectType")]
+
+public class ProjectTypeEntity
+{
+    [Key]
+    public Guid ProjectTypeId { get; set; }
+    
+    [Display(Name ="نوع پروژه")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(150 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Title { get; set; }
+
+    #region Relation
+    public List<Project_ProjectTypeEntity> ProjectProjectTypes { get; set; }
+    
+    #endregion
+}
