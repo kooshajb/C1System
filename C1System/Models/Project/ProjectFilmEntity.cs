fb5aced4ce9f2b96ccc46cac1f79a08ace41bc7d
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 15:26:21 +0430

    add ProjectFilmEntity

---
+++ C1System/Models/Project/ProjectFilmEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System;
+
+[Table("ProjectFilm")]
+public class ProjectFilmEntity
+{
+    [Key]
+    public Guid ProjectFilmId { get; set; }
+    
+    [Display(Name ="عنوان فیلم")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(100 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Title { get; set; }
+    
+    [Display(Name ="زیرعنوان فیلم")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(150 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string SubTitle { get; set; }
+    
+    //todo video files
+}
