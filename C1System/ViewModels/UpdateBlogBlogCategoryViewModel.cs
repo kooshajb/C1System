32d9bfc4acd5df113263d98101db76487e0bb3cb
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-19 00:50:17 +0430

    fix UpdateBlogBlogCategoryViewModel

---
+++ C1System/ViewModels/UpdateBlogBlogCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.ViewModels;
+
+public class UpdateBlogBlogCategoryViewModel
+{
+    public Guid BlogId { get; set; }
+
+    [Display(Name = "عنوان پست")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    public string BlogTitle { get; set; }
+
+    public Guid BlogBlogCategoryId { get; set; }
+    
+    public Guid BlogCategoryId { get; set; }
+}
