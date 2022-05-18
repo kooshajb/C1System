147be886d767b6e579bbd721919e5df4159df775
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 18:24:01 +0430

    fix PodcastEntity

--- C1System/Models/Podcast/PodcastEntity.cs
+++ C1System/Models/Podcast/PodcastEntity.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using C1System.Media;
 
 namespace C1System;
 
@@ -25,22 +26,13 @@ public class PodcastEntity :BannerEntity
     [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
     [MaxLength(5 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
     public string StudyTime { get; set; }
-    
-    [Display(Name ="تصویر شاخص")]
-    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
-    public string FeatureImage { get; set; }
-    
+
     [Display(Name = "توضیحات پادکست")]
     [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
     [MinLength(200, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
     [MaxLength(1000, ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
     public string Description { get; set; }
 
-    [Display(Name = "فایل صوتی")]
-    [Required(ErrorMessage = "لطفا {0} را وارد کنید .")]
-    [MinLength(50, ErrorMessage = "تعداد {0} نباید کمتر از {1} باشد.")]
-    public string Audio { get; set; }
-    
     [Display(Name ="علاقه مندی")]
     public bool? IsLike { get; set; }
     
@@ -56,6 +48,8 @@ public class PodcastEntity :BannerEntity
     #region Relation
     
     public List<Tag_PodcastEntity> TagPodcasts { get; set; }
+    // public List<MediaEntity> PodcastFeatureImg { get; set; }
+    // public List<MediaEntity> PodcastAudio { get; set; }
 
     #endregion
     // public BookMarkEntity BookMark { get; set; }
