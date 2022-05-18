6d455257081122f13dacf747de20ae1034443d0a
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 17:51:35 +0430

    fix ProjectDocEntity

--- C1System/Models/Project/ProjectDocEntity.cs
+++ C1System/Models/Project/ProjectDocEntity.cs
@@ -9,6 +9,15 @@ public class ProjectDocEntity
     [Key]
     public Guid ProjectDocId { get; set; }
     
+    [Display(Name ="عنوان فایل مستند")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(100 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Title { get; set; }
     
+    #region Relation
+        
     
+
+    #endregion
 }
