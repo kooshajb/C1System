7bcf19ba510943282ea67c8b99e99933b4a81837
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 23:19:42 +0430

    add BlogCategoryDto

---
+++ C1System/Dtos/Blog/BlogCategoryDto.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using C1System;
+
+public class GetBlogCategoryDto
+{
+    public Guid BlogCategoryId { get; set; }
+    public string Title { get; set; }
+}
+
+public class AddBlogCategoryDto
+{
+    public string Title { get; set; }
+}
+
+public class UpdateBlogCategoryDto
+{
+    public Guid BlogCategoryId { get; set; }
+    public string Title { get; set; }
+}
+
+public class AutoMapperBlogCategory : Profile {
+    public AutoMapperBlogCategory() {
+        CreateMap<BlogCategoryEntity, AddBlogCategoryDto>().ReverseMap();
+        CreateMap<BlogCategoryEntity, UpdateBlogCategoryDto>().ReverseMap();
+        CreateMap<BlogCategoryEntity, GetBlogCategoryDto>().ReverseMap();
+        CreateMap<AddBlogCategoryDto, GetBlogCategoryDto>().ReverseMap();
+        CreateMap<UpdateBlogCategoryDto, GetBlogCategoryDto>().ReverseMap();
+    }
+}
