fd113deeb4ecc3ed70ce14ee6c2c3d0ccfe98ba4
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 15:26:45 +0430

    add Project_ProjectTypeEntity

---
+++ C1System/Models/Project/Project_ProjectTypeEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System;
+
+[Table("Project_ProjectType")]
+public class Project_ProjectTypeEntity
+{
+    [Key]
+    public Guid ProjectProjectTypeId { get; set; }
+    
+    public Guid ProjectId { get; set; }
+    public Guid ProjectTypeId { get; set; }
+    
+    #region Relation
+    
+    [ForeignKey("ProjectId")]
+    public ProjectEntity Project { get; set; }
+    
+    [ForeignKey("ProjectTypeId")]
+    public ProjectTypeEntity ProjectType { get; set; }
+
+    #endregion
+}
