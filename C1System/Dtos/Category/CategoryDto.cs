5d3f4d1c6228af2ecc348030fa4102c1a34d73d2
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 17:52:19 +0430

    fix CategoryDto

--- C1System/Dtos/Category/CategoryDto.cs
+++ C1System/Dtos/Category/CategoryDto.cs
@@ -7,31 +7,38 @@ public class GetCategoryDto
     public string Title { get; set; }
     public string SubTitle { get; set; }
     public string Description { get; set; }
-    public string IconImage { get; set; }
     public string IntroDescription { get; set; }
-    public string IntroImage { get; set; }
     public string BannerTitle { get; set; }
     public string BannerDescription { get; set; }
-    public string BannerImage { get; set; }
-    public string IconMenuImage { get; set; }
     public string? VideoIntro { get; set; }
     public Guid? ParentId { get; set; }
     public bool IsDelete { get; set; }
 }    
 
 
-public class AddUpdateCategoryDto
-{
+public class AddCategoryDto
+{ 
     public string Title { get; set; }
     public string SubTitle { get; set; }
     public string Description { get; set; }
-    public string IconImage { get; set; }
     public string IntroDescription { get; set; }
-    public string IntroImage { get; set; }
     public string BannerTitle { get; set; }
     public string BannerDescription { get; set; }
-    public string BannerImage { get; set; }
-    public string IconMenuImage { get; set; }
+    public string? VideoIntro { get; set; }
+    public Guid? ParentId { get; set; }
+    public bool IsDelete { get; set; }
+}
+
+
+public class UpdateCategoryDto
+{
+    public Guid CategoryId { get; set; }
+    public string Title { get; set; }
+    public string SubTitle { get; set; }
+    public string Description { get; set; }
+    public string IntroDescription { get; set; }
+    public string BannerTitle { get; set; }
+    public string BannerDescription { get; set; }
     public string? VideoIntro { get; set; }
     public Guid? ParentId { get; set; }
     public bool IsDelete { get; set; }
@@ -39,9 +46,12 @@ public class AddUpdateCategoryDto
 
 
 public class AutoMapperCategory : Profile {
-    public AutoMapperCategory() {
-        CreateMap<CategoryEntity, AddUpdateCategoryDto>().ReverseMap();
+    public AutoMapperCategory()
+    {
+        CreateMap<CategoryEntity, AddCategoryDto>().ReverseMap();
+        CreateMap<CategoryEntity, UpdateCategoryDto>().ReverseMap();
         CreateMap<CategoryEntity, GetCategoryDto>().ReverseMap();
-        CreateMap<AddUpdateCategoryDto, GetCategoryDto>().ReverseMap();
+        CreateMap<AddCategoryDto, GetCategoryDto>().ReverseMap();
+        CreateMap<UpdateCategoryDto, GetCategoryDto>().ReverseMap();
     }
 }
