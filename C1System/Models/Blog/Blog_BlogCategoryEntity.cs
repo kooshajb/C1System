b629a1182e75b431e5e8b2d46015fc86eb5bdb77
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 23:19:03 +0430

    add Blog_BlogCategoryEntity

---
+++ C1System/Models/Blog/Blog_BlogCategoryEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System;
+
+[Table("Blog_BlogCategory")]
+public class Blog_BlogCategoryEntity
+{
+    [Key]
+    public int Blog_BlogCategoryId { get; set; }
+    
+    public Guid BlogId { get; set; }
+    public Guid BlogCategoryId { get; set; }
+
+    #region Relation
+    
+    [ForeignKey("BlogId")]
+    public BlogEntity Blog { get; set; }
+    
+    [ForeignKey("BlogCategoryId")]
+    public BlogCategoryEntity BlogCategory { get; set; }
+
+    #endregion
+}
