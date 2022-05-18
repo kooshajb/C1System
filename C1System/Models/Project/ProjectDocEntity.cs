a9a3ebae96560cba875586ca310bec585e39fad7
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 15:26:38 +0430

    fix ProjectDocEntity

---
+++ C1System/Models/Project/ProjectDocEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System;
+
+[Table("ProjectDoc")]
+public class ProjectDocEntity
+{
+    [Key]
+    public Guid ProjectDocId { get; set; }
+    
+    
+    
+}
