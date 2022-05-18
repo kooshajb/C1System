2a80ed1d089f3f26173ab9143d10d2ef4ef8af5a
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-18 13:25:12 +0430

    add ConsultingDto

---
+++ C1System/Dtos/Consulting/ConsultingDto.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using C1System;
+
+public class GetConsultingDto
+{
+    public Guid ConsultingId { get; set; }
+    public string FullName { get; set; }
+    public string MobileNumber { get; set; }
+    // public bool? IsRead { get; set; }
+}
+
+public class AddConsultingDto
+{ 
+    public string FullName { get; set; }
+    public string MobileNumber { get; set; }
+    // public bool? IsRead { get; set; }
+}
+
+public class UpdateConsultingDto
+{
+    public Guid ConsultingId { get; set; }
+    public string FullName { get; set; }
+    public string MobileNumber { get; set; }
+    // public bool? IsRead { get; set; }
+}
+
+public class AutoMapperConsulting : Profile {
+    public AutoMapperConsulting() {
+        CreateMap<ConsultingEntity, AddConsultingDto>().ReverseMap();
+        CreateMap<ConsultingEntity, UpdateConsultingDto>().ReverseMap();
+        CreateMap<ConsultingEntity, GetConsultingDto>().ReverseMap();
+        CreateMap<AddConsultingDto, GetConsultingDto>().ReverseMap();
+        CreateMap<UpdateConsultingDto, GetConsultingDto>().ReverseMap();
+    }
+}
