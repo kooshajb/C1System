e6b51b2299c52a2bda389f6aa2419c244833f352
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-19 12:45:13 +0430

    fix ProjectFilmEntity

--- C1System/Models/Project/ProjectFilmEntity.cs
+++ C1System/Models/Project/ProjectFilmEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
 
 namespace C1System;
 
@@ -20,6 +21,5 @@ public class ProjectFilmEntity
     [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
     [MaxLength(150 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
     public string SubTitle { get; set; }
-    
-    //todo video files
+
 }
