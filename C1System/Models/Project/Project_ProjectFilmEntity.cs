1c1158ef7688e30732e521298798444b3d7cbc3f
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-19 12:45:26 +0430

    fix Project_ProjectFilmEntity

---
+++ C1System/Models/Project/Project_ProjectFilmEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace C1System;
+
+[Table("Project_ProjectFilm")]
+public class Project_ProjectFilmEntity
+{
+    [Key]
+    public Guid ProjectProjectFilmId { get; set; }
+    
+    public Guid ProjectId { get; set; }
+    public Guid ProjectFilmId { get; set; }
+    
+    #region Relation
+    
+    [ForeignKey("ProjectId")]
+    public ProjectEntity Project { get; set; }
+    
+    [ForeignKey("ProjectFilmId")]
+    public ProjectFilmEntity ProjectFilm { get; set; }
+
+    #endregion
+}
