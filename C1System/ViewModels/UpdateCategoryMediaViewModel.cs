c42e9da9256cd5dfdd248221dac212539bb46f66
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-19 12:44:58 +0430

    add  UpdateCategoryMediaViewModel

---
+++ C1System/ViewModels/UpdateCategoryMediaViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.ViewModels;
+
+public class UpdateCategoryMediaViewModel
+{
+    public Guid CategoryId { get; set; }
+
+    public Guid MediaId { get; set; }
+    public string FileName { get; set; }
+}
