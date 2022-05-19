8fae550a8aeade3b3077ac770185507bea121faf
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-19 12:45:07 +0430

    fix ProjectStatusEntity

--- C1System/Models/Project/ProjectStatusEntity.cs
+++ C1System/Models/Project/ProjectStatusEntity.cs
@@ -9,5 +9,15 @@ public class ProjectStatusEntity
     [Key]
     public Guid ProjectStatusId { get; set; }
     
+    [Display(Name ="وضعیت پروژه")]
+    [Required(ErrorMessage ="لطفا {0} را وارد کنید .")]
+    [MinLength(5 , ErrorMessage ="تعداد {0} نباید کمتر از {1} باشد.")]
+    [MaxLength(150 , ErrorMessage = "تعداد {0} نباید بیشتر از {1} باشد.")]
+    public string Title { get; set; }
+
+    #region Relation
+
+    public List<ProjectEntity> Projects { get; set; }
     
+    #endregion
 }
