3b3b0eeaec40b61fd31564e46a8d5d269e67f4fd
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-19 17:32:44 +0430

    fix UpdateTechMediaViewModel

---
+++ C1System/ViewModels/UpdateTechMediaViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.ViewModels;
+
+public class UpdateTechMediaViewModel
+{
+    public Guid TechnologyId { get; set; }
+
+    public Guid MediaId { get; set; }
+    public string FileName { get; set; }
+}
