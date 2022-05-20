da9db084aba3af991901aaf4206965367b43d6a5
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-20 18:09:30 +0430

    add UpdatePodcastMediaViewModel

---
+++ C1System/ViewModels/UpdatePodcastMediaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.ViewModels;
+
+public class  UpdatePodcastMediaViewModel
+{
+
+    public Guid PodcastId { get; set; }
+
+    public Guid MediaId { get; set; }
+    public string FileName { get; set; }
+}
