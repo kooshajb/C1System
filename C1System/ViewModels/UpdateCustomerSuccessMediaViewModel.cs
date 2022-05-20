e767aa3a7e9c68e970131b5c6a49f3f6b2d4cb32
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-20 21:12:29 +0430

    add UpdateCustomerSuccessMediaViewModel

---
+++ C1System/ViewModels/UpdateCustomerSuccessMediaViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.ViewModels;
+
+public class UpdateCustomerSuccessMediaViewModel
+{
+    public Guid CustomerSuccessId { get; set; }
+    public Guid MediaId { get; set; }
+    public string FileName { get; set; }
+}
