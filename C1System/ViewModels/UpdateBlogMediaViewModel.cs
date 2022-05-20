a873df666196c562c3257be2a867e913bba4586b
simaheidariesf <simaheidariesfahani@gmail.com>
2022-05-20 21:12:35 +0430

    add UpdateBlogMediaViewModel

---
+++ C1System/ViewModels/UpdateBlogMediaViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C1System.ViewModels;
+
+public class UpdateBlogMediaViewModel
+{
+    public Guid BlogId { get; set; }
+
+    public Guid MediaId { get; set; }
+    public string FileName { get; set; }
+}
